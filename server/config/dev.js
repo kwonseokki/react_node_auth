5eb981aeeaf2bf5f2f18d1d3669a4934b898aa32
KSK <101062450+kwonseokki@users.noreply.github.com>
2022-08-29 22:28:47 +0900

    Delete server/config directory

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:1234@react-node.bgg018w.mongodb.net/?retryWrites=true&w=majority",
-};
--- server/config/key.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  // 배포모드
-  module.exports = require("./prod"); // prod.js 내보내기 db주소 감춰있음
-} else {
-  module.exports = require("./dev"); // dev.js 설정 내보내기
-}
-
-process.env.USER = "dd";
